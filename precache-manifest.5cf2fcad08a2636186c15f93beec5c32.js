fd63c89c3564511aea4a2e8506468609edfd58ce
jasonNikolaou <nikolaouiasonas13@gmail.com>
2020-08-07 19:48:50 +0300

    Updates

---
+++ precache-manifest.5cf2fcad08a2636186c15f93beec5c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f51c020e442f3fe56dc2e2c3e89c49c",
+    "url": "/ECE_Appathon/index.html"
+  },
+  {
+    "revision": "765c2002e9faa6aab739",
+    "url": "/ECE_Appathon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "765c2002e9faa6aab739",
+    "url": "/ECE_Appathon/static/js/2.e53f5a8c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ECE_Appathon/static/js/2.e53f5a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0dc9f4aef78ca1a73b",
+    "url": "/ECE_Appathon/static/js/main.1089afd1.chunk.js"
+  },
+  {
+    "revision": "7103308ee3c1e2766264",
+    "url": "/ECE_Appathon/static/js/runtime-main.d9f3cb29.js"
+  }
+]);
